f817390b355a0ba1d05c74668a7a49bc9576df08
lenovo <lenovo@DESKTOP-REKBERN>
2017-08-05 12:41:11 -0500

    hola

---
+++ src/entrada/y/salida/EntradaYSalida.java
@@ -0,0 +1,22 @@
+
+package entrada.y.salida;
+
+import java.util.Scanner;
+
+public class EntradaYSalida {
+
+
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);//Se crea un objeto 
+       String cadena;
+       System.out.println("Digite una cadena: ");
+       cadena = entrada.nextLine(); // .nextLine lee toda la linea
+        System.out.println(cadena);
+        
+       char caracter;
+        System.out.println("DIGITE UN CARACTER: ");
+        caracter = entrada.next().charAt(0);
+        System.out.println(caracter);
+    }
+    
+}
